0d0da3b467075707851b0ab7167e328883f91b45
Jawaria Tariq <150448000+mjawariatariq@users.noreply.github.com>
2025-01-16 20:56:27 +0500

    Create views.py

---
+++ Python Framework Projects/New/NetworkChatApplication/rtc_app/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from rtc_app.models import Message, Room
+from django.contrib.auth.models import User
+
+from django.shortcuts import render
+from .models import Room,Message
+
+def rooms(request):
+    rooms=Room.objects.all()
+    return render(request, "rooms.html",{"rooms":rooms})
+
+from django.http import Http404
+
+def room(request, slug):
+    try:
+        room_name = Room.objects.get(slug=slug).name
+    except Room.DoesNotExist:
+        raise Http404("Room not found")
+    messages = Message.objects.filter(room__slug=slug)
+    return render(request, "room.html", {"room_name": room_name, "slug": slug, 'messages': messages})
+
+def user_logout(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('login')
+    return redirect('rooms')
+
+def room_view(request, room_slug):
+    room = Room.objects.get(slug=room_slug)
+    messages = room.message_set.all()  # Assuming the messages are related to rooms
+    return render(request, 'chat_room.html', {
+        'room_name': room.name,
+        'messages': messages,
+    })
