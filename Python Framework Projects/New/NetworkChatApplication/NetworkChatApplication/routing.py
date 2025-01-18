0325d757b9c5ec97b71a658b02b8d20b79c42d8a
Jawaria Tariq <150448000+mjawariatariq@users.noreply.github.com>
2025-01-18 15:38:21 +0500

    Update routing.py

--- Python Framework Projects/New/NetworkChatApplication/NetworkChatApplication/routing.py
+++ Python Framework Projects/New/NetworkChatApplication/NetworkChatApplication/routing.py
@@ -1,8 +1,12 @@
-from django.urls import path , include,re_path
+# Django se URL patterns aur include, re_path ko import kar rahe hain
+from django.urls import path, include, re_path
+# Apni ChatConsumer ko import kar rahe hain
 from .consumers import ChatConsumer
 
-
+# WebSocket URLs ko define kar rahe hain
 websocket_urlpatterns = [
-	path("<room_slug>" , ChatConsumer.as_asgi()) ,
+    # Room slug ko path mein pass kar rahe hain aur ChatConsumer ko as_asgi() ke saath use kar rahe hain
+    path("<room_slug>", ChatConsumer.as_asgi()),
+    # Regular expression ke zariye room_slug ko capture kar rahe hain aur ChatConsumer ko as_asgi() ke saath use kar rahe hain
     re_path(r'^ws/(?P<room_slug>[^/]+)/$', ChatConsumer.as_asgi()),
 ]
