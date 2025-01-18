41ea31c33ac8d69c3d4670187405420bff68166d
Jawaria Tariq <150448000+mjawariatariq@users.noreply.github.com>
2025-01-18 15:39:05 +0500

    Update urls.py

--- Python Framework Projects/New/NetworkChatApplication/NetworkChatApplication/urls.py
+++ Python Framework Projects/New/NetworkChatApplication/NetworkChatApplication/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+
 urlpatterns = [
+    # Admin site ke liye URL path define kar rahe hain
     path('admin/', admin.site.urls),
+    # Accounts app ke URLs ko include kar rahe hain (auth related URLs)
     path("accounts/", include("django.contrib.auth.urls")),
-    path('',include('rtc_app.urls')),
+    # rtc_app ke URLs ko include kar rahe hain
+    path('', include('rtc_app.urls')),
 ]
