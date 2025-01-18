ec488eb390fa1a4d0dc8d8ba34d8b3d6cac1da1a
Jawaria Tariq <150448000+mjawariatariq@users.noreply.github.com>
2025-01-18 15:39:18 +0500

    Update wsgi.py

--- Python Framework Projects/New/NetworkChatApplication/NetworkChatApplication/wsgi.py
+++ Python Framework Projects/New/NetworkChatApplication/NetworkChatApplication/wsgi.py
@@ -1,7 +1,17 @@
+
+"""
+WSGI config for wechatpp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rtc_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NetworkChatApplication.settings')
 
 application = get_wsgi_application()
