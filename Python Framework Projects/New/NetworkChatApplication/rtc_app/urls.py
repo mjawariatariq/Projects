2231c2d75ca707809f246144049ab5738d8f0fc8
Jawaria Tariq <150448000+mjawariatariq@users.noreply.github.com>
2025-01-18 15:42:14 +0500

    Update urls.py

--- Python Framework Projects/New/NetworkChatApplication/rtc_app/urls.py
+++ Python Framework Projects/New/NetworkChatApplication/rtc_app/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path, include
+from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
+# URL patterns define kar rahe hain
 urlpatterns = [
+    # Rooms view ke liye URL path
     path("", views.rooms, name="rooms"),
-    path("<str:slug>", views.room, name="room"),
-    path("logout/", views.user_logout, name="logout"),
+    # Login view ke liye URL path
     path("login/", auth_views.LoginView.as_view(), name="login"),
+    # Logout view ke liye URL path, logout hone ke baad login page pe redirect hoga
+    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
+    # Room ke liye URL path, slug ke zariye specific room ko access kar rahe hain
+    path("<str:slug>", views.room, name="room"),
 ]
