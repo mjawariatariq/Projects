5e9038b3e786238aadb63811adf2323081795564
Jawaria Tariq <150448000+mjawariatariq@users.noreply.github.com>
2025-04-26 00:51:11 +0500

    Delete Python Framework Projects/NetworkChatApplication directory

--- Python Framework Projects/NetworkChatApplication/NetworkChatApplication/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('rtc_app.urls')),  # Connects to the rtc_app routes
-]
--- Python Framework Projects/NetworkChatApplication/rtc_app/admin.py
+++
@@ -1,6 +0,0 @@
-# Django admin mein models ko register karna
-from django.contrib import admin
-from .models import Room, Message
-
-admin.site.register(Room)
-admin.site.register(Message)
